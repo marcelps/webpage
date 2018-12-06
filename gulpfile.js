031af754d7d4757418ec23eaf7da099df956f810
marcelps <marcelps@users.noreply.github.com>
2018-12-06 16:44:32 +0100

    chore: gulp config change

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const {series, parallel, src, dest, watch} = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
 
 watch('style/*.css', minifyCss);
+watch('templates/**/*.ejs', ejsTemplate);
+
+function ejsTemplate() {
+    return src('templates/pages/*.ejs')
+        .pipe(ejs({}, {}, {ext: '.html'}))
+        .pipe(dest('public'));
+}
 
 function javascript() {
     return src(
         [
             'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map',
             'node_modules/jquery/dist/jquery.slim.min.js',
             'node_modules/owl.carousel/dist/owl.carousel.min.js'
         ]
@@ -19,6 +28,7 @@ function css() {
     return src(
         [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css.map',
             'node_modules/owl.carousel/dist/assets/owl.carousel.min.css',
             'node_modules/owl.carousel/dist/assets/owl.theme.default.min.css'
         ]
@@ -33,4 +43,4 @@ function minifyCss() {
         .pipe(dest('public/css'));
 }
 
-exports.default = series(minifyCss, parallel(javascript, css));
+exports.default = series(minifyCss, ejsTemplate, parallel(javascript, css));
